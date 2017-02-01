fd6a95cb27240df7bca90f6933c7a179ab1f0453
Cruz Kerver <cruzkerver@cruzs-macbook-pro.local>
2017-02-01 10:57:38 -0800

    Initial Commit

---
+++ HMWK1_CardShuffling/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HMWK1_CardShuffling
+//
+//  Created by Cruz Kerver on 2/1/17.
+//  Copyright © 2017 Autonomy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
